682c8889ac76c09f49f1444f037116577529642f
up201905853 <up201905853@fe.up.pt>
2021-05-15 18:57:04 +0100

    5 queries, 5 left

---
+++ int2.sql
@@ -0,0 +1,15 @@
+.mode columns
+.headers on
+.nullvalue NULL
+
+
+--Esta query destina-se a obter todos os filmes que um dado utilizador (neste caso 1)
+-- ainda não tenha visto
+
+SELECT titulo
+FROM Conteudo JOIN Filme ON Conteudo.id = Filme.idConteudo
+WHERE Conteudo.id not in (
+	SELECT idConteudo
+	FROM Historico
+	WHERE Historico.idPerfil = 1
+	)
